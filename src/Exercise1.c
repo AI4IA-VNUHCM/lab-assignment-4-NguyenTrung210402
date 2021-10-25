4d6815e414d416925b1be5a18f852bc66e5c206e
NguyenTrung210402 <91875395+NguyenTrung210402@users.noreply.github.com>
2021-10-25 14:36:19 +0700

    dt

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -11,19 +11,52 @@ ______________________________________
 |____________________________________|
 */
 
+//  C++ code for Pascal's Triangle
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-void Ex1(int n){
-	//Your codes here
-    
+ 
+// See https://www.geeksforgeeks.org/space-and-time-efficient-binomial-coefficient/
+// for details of this function
+int binomialCoeff(int n, int k);
+ 
+// Function to print first
+// n lines of Pascal's
+// Triangle
+void printPascal(int n)
+{
+    // Iterate through every line and
+    // print entries in it
+    for (int line = 0; line < n; line++)
+    {
+        // Every line has number of
+        // integers equal to line
+        // number
+        for (int i = 0; i <= line; i++)
+            printf("%d ",
+                    binomialCoeff(line, i));
+        printf("\n");
+    }
 }
-
-int main(int argc, char *argv[]) {
-	//testing variable, applying it to your algorithm for auto-evaluating
-	int testcase = atoi(argv[1]);
-	
-	Ex1(testcase);
-	return 0;
+ 
+// See https://www.geeksforgeeks.org/space-and-time-efficient-binomial-coefficient/
+// for details of this function
+int binomialCoeff(int n, int k)
+{
+    int res = 1;
+    if (k > n - k)
+    k = n - k;
+    for (int i = 0; i < k; ++i)
+    {
+        res *= (n - i);
+        res /= (i + 1);
+    }
+     
+    return res;
+}
+ 
+// Driver program
+int main()
+{
+    int n = 4;
+    printPascal(n);
+    return 0;
 }
